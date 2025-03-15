1f45bce5c3431f2d66328d9b7cb6390266ce1e82
Mdara1122 <marcosalazar4455@icloud.com>
2025-03-14 22:36:15 -0600

    a

--- mapa.php
+++ mapa.php
@@ -5,7 +5,7 @@
     <section id="mapa" class="mt-5">
         <h2 class="mb-4">Encuéntranos</h2>
         <div id="map" style="width: 100%; height: 400px;"></div>
-        <iframe class="col-sm-12" src=<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3929.215179409634!2d-84.13157172597498!3d9.999076673052658!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa0faeee3c54c71%3A0x7b6dfa32a05d9652!2sLiceo%20Ing.%20Samuel%20S%C3%A1enz%20Flores!5e0!3m2!1ses-419!2scr!4v1742012812511!5m2!1ses-419!2scr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe class="col-sm-12" src=<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3929.215179409634!2d-84.13157172597498!3d9.999076673052658!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa0faeee3c54c71%3A0x7b6dfa32a05d9652!2sLiceo%20Ing.%20Samuel%20S%C3%A1enz%20Flores!5e0!3m2!1ses-419!2scr!4v1742012812511!5m2!1ses-419!2scr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></iframe>
     </section>
 </main>
 
