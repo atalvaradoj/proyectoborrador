40e5d2c48f510687edb44e99c33346fffc40bed0
Jerald Alvarado Vanegas <at_alvaradoj@outlook.com>
2025-03-14 22:39:57 -0600

    cc

--- mapa.php
+++ mapa.php
@@ -5,7 +5,7 @@
     <section id="mapa" class="mt-5">
         <h2 class="mb-4">Encuéntranos</h2>
         <div id="map" style="width: 100%; height: 400px;"></div>
-        <iframe class="col-sm-12" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3930.417033734353!2d-84.07105932589067!3d9.899181690201141!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa0e3035ccd6865%3A0x752002c761e99e19!2sCin%C3%A9polis%20Desamparados!5e0!3m2!1ses-419!2scr!4v1739330034090!5m2!1ses-419!2scr" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe class="col-sm-12" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3929.216294191697!2d-84.13070692426969!3d9.998984473050756!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa0faeed1d8162f%3A0x3335d23cdf9ef33b!2sParque%20Samuel%20Saenz!5e0!3m2!1ses-419!2scr!4v1742012935297!5m2!1ses-419!2scr" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </section>
 </main>
 
