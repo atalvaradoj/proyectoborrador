b04418aa613c8ac1722abe91c28d0f7735af4af8
Jerald Alvarado Vanegas <at_alvaradoj@outlook.com>
2025-03-17 21:42:51 -0600

    cc

--- index.php
+++ index.php
@@ -63,35 +63,6 @@
             </div>
     </section>
 
-    <section>
-
-        <div class="container">
-            <div class="header">
-                <h1>¿Por qué elegir una escuela?</h1>
-            </div>
-
-            <div class="content">
-                <h2>Elige con sabiduría, invierte en tu futuro</h2>
-                <p>Elegir una escuela es una de las decisiones más importantes que un estudiante y su familia pueden tomar, ya que el entorno educativo influye directamente en el desarrollo académico y personal. Una buena escuela no solo proporciona una educación de calidad, sino que también fomenta el crecimiento integral de los estudiantes, ofreciendo programas que impulsan su creatividad, habilidades sociales y deportivas. Además, una escuela con un enfoque en la innovación y el uso de tecnología prepara a los estudiantes para enfrentar los desafíos del futuro. Al elegir una escuela, es esencial considerar aspectos como la calidad de sus docentes, los valores que promueve, la infraestructura, las actividades extracurriculares y, por supuesto, el ambiente de respeto y seguridad que brinda a sus alumnos.</p>
-                <a href="registro.html" class="btn btn-primary">Explora nuestras opciones educativas</a>
-            </div>
-        </div>
-
-    </section>
-        <!-- Sección del Mapa -->
-        <section id="mapa" class="mb-5">
-        <h2 class="mb-4 text-center">Encuéntranos</h2>
-        <div class="row">
-            <div class="col-12">
-                <iframe class="w-100"
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3929.215179409634!2d-84.13157172597502!3d9.999076673052658!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa0faeee3c54c71%3A0x7b6dfa32a05d9652!2sLiceo%20Ing.%20Samuel%20S%C3%A1enz%20Flores!5e0!3m2!1ses-419!2scr!4v1742171660337!5m2!1ses-419!2scr"
-                    height="400" style="border:0;" allowfullscreen="" loading="lazy"
-                    referrerpolicy="no-referrer-when-downgrade">
-                </iframe>
-            </div>
-        </div>
-    </section>
-
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
 
